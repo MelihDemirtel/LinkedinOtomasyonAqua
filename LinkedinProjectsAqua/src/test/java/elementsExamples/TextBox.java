314b6e174de35db67b86a564aabec3d1bfec8070
MelihDemirtel <melihcandemirtel@gmail.com>
2023-01-08 17:47:11 +0300

    examplesProject done

--- LinkedinProjectsAqua/src/test/java/elementsExamples/TextBox.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/TextBox.java
@@ -1,4 +1,74 @@
 package elementsExamples;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class TextBox {
+    public String expectedValidationText = "Please select an item from left to start practice.";
+    public String fullName = "Test";
+    public String email = "test@deneme.com";
+    public String currentAddress = "deneme caddesi";
+    public String permanentAddress = "test deneme";
+    public int milis = 2000;
+    public String scrollDown = "window.scrollBy(0,350)";//Sayfayı aşağı kaydırabilmek için
+
+    TextBoxPage textBoxPage = new TextBoxPage();
+
+    @BeforeClass
+    public static void setUpAll() {
+        Configuration.browserSize = "1920x1080";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeMethod
+    public void setUp() throws InterruptedException {
+        open("https://demoqa.com");
+        Thread.sleep(milis);
+    }
+
+    @AfterClass
+    public void tearDown(){ closeWebDriver();}
+
+    @Test
+    public void test1() throws InterruptedException {
+
+        textBoxPage.elementsButton.click();
+        Thread.sleep(milis);
+
+        textBoxPage.actualValidationText.isDisplayed();
+        Assert.assertEquals(textBoxPage.actualValidationText.getText(), expectedValidationText);
+
+        textBoxPage.textBoxButton.click();
+        Thread.sleep(milis);
+
+        textBoxPage.fullNameLabelText.isDisplayed();
+
+        textBoxPage.fullNameLabel.sendKeys(fullName);
+        Thread.sleep(milis);
+        textBoxPage.emailLabel.sendKeys(email);
+        Thread.sleep(milis);
+        textBoxPage.currentAddressLabel.sendKeys(currentAddress);
+        Thread.sleep(milis);
+        textBoxPage.permanentAddressLabel.sendKeys(permanentAddress);
+        Thread.sleep(milis);
+
+        textBoxPage.submitButton.click();
+        executeJavaScript(scrollDown);//Sayfa aşağı kaydırılır.
+        Thread.sleep(milis);
+
+        textBoxPage.resultArea.isDisplayed();
+        Assert.assertEquals(textBoxPage.actualName.getText(), "Name:" + fullName);
+        Assert.assertEquals(textBoxPage.actualEmail.getText(), "Email:" + email);
+        Assert.assertEquals(textBoxPage.actualCurrentAddress.getText(), "Current Address :" + currentAddress);
+        Assert.assertEquals(textBoxPage.actualPermanentAddress.getText(), "Permananet Address :" + permanentAddress);
+    }
+
 }
--- LinkedinProjectsAqua/src/test/java/elementsExamples/TextBoxPage.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/TextBoxPage.java
@@ -1,4 +1,39 @@
 package elementsExamples;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class TextBoxPage {
+    //public SelenideElement elementsButton = $(By.xpath("//h5[text()='Elements']"));
+    public SelenideElement elementsButton = $x("//h5[text()='Elements']");
+
+    public SelenideElement actualValidationText = $(By.cssSelector(".col-12.mt-4.col-md-6"));
+
+    public SelenideElement textBoxButton = $x("//span[text()='Text Box']");
+
+    public SelenideElement textBoxButtonCss = $(By.cssSelector(".text"));
+
+    public SelenideElement fullNameLabelText = $(By.id("userName-label"));
+
+    public SelenideElement fullNameLabel = $(By.id("userName"));
+
+    public SelenideElement emailLabel = $(By.id("userEmail"));
+
+    public SelenideElement currentAddressLabel = $(By.id("currentAddress"));
+
+    public SelenideElement permanentAddressLabel = $(By.id("permanentAddress"));
+
+    public SelenideElement submitButton = $(By.id("submit"));
+
+    public SelenideElement resultArea = $(By.cssSelector(".border.col-md-12.col-sm-12"));
+
+    public SelenideElement actualName = $x("//p[@id='name']");
+
+    public SelenideElement actualEmail = $(By.id("email"));
+    public SelenideElement actualCurrentAddress = $x("//p[@id='currentAddress']");
+    public SelenideElement actualPermanentAddress = $x("//p[@id='permanentAddress']");
+
 }
