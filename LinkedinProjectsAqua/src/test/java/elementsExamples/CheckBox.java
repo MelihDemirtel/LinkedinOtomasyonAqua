bb4796ae209ce3e61bfd97ef4a4af17ce5ebd115
MelihDemirtel <melihcandemirtel@gmail.com>
2023-01-14 17:30:58 +0300

    checkBox project done

--- LinkedinProjectsAqua/src/test/java/elementsExamples/CheckBox.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/CheckBox.java
@@ -1,8 +1,10 @@
 package elementsExamples;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -12,6 +14,9 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CheckBox {
+    public String expectedFirstIndexHome = "home";
+    public String actualFirstIndexHome;
+    int milis = 1000;
    CheckBoxPage checkBoxPage = new CheckBoxPage();
     @BeforeClass
     public static void setUpAll() {
@@ -30,7 +35,46 @@ public class CheckBox {
     }
 
     @Test
-    public void homePage() {
+    public void checkBoxPage() throws InterruptedException {
+        checkBoxPage.elementsButton.click();
+        Thread.sleep(milis);
 
+        checkBoxPage.checkBoxButton.click();
+        Thread.sleep(milis);
+
+        boolean isSelectedHome = checkBoxPage.checkBoxHomeButton.isSelected();
+
+        if(isSelectedHome){
+            System.out.println("CheckBox is already selected");
+        }else{
+            checkBoxPage.checkBoxHomeButton.click();
+            System.out.println("CheckBox is selected now");
+        }
+        Thread.sleep(milis);
+
+        for (SelenideElement result : checkBoxPage.resultHome){
+            System.out.println(result.getText());
+        }
+        actualFirstIndexHome = checkBoxPage.resultHome.get(0).getText();
+        Assert.assertEquals(actualFirstIndexHome, expectedFirstIndexHome);
+
+        checkBoxPage.toggleButton1.click();
+        Thread.sleep(milis);
+
+        Assert.assertTrue(checkBoxPage.checkBoxDesktop.isDisplayed());
+        Assert.assertTrue(checkBoxPage.checkBoxDocument.isDisplayed());
+        Assert.assertTrue(checkBoxPage.checkBoxDownloads.isDisplayed());
+
+        checkBoxPage.checkBoxHomeButton.click();
+        Thread.sleep(milis);
+
+        checkBoxPage.checkBoxDesktop.click();
+        Thread.sleep(milis);
+
+        if (isSelectedHome){
+            System.out.println("CheckBox is already selected");
+        }else{
+            System.out.println("CheckBox is NOT selected");
+        }
     }
 }
--- LinkedinProjectsAqua/src/test/java/elementsExamples/CheckBoxPage.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/CheckBoxPage.java
@@ -1,4 +1,20 @@
 package elementsExamples;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class CheckBoxPage {
+    public SelenideElement elementsButton = $x("//h5[text()='Elements']");
+    public SelenideElement checkBoxButton = $x("//span[text()='Check Box']");
+    public SelenideElement checkBoxHomeButton = $(By.className("rct-title"));
+    public ElementsCollection resultHome = $$x(("//span[@class='text-success']"));
+    public SelenideElement toggleButton1 = $x("(//button[@title='Toggle'])[1]");
+    public SelenideElement checkBoxDesktop = $x("//span[contains(text(),'Desktop')]");
+    public SelenideElement checkBoxDocument = $x("//span[contains(text(),'Document')]");
+    public SelenideElement checkBoxDownloads = $x("//span[contains(text(),'Downloads')]");
 }
