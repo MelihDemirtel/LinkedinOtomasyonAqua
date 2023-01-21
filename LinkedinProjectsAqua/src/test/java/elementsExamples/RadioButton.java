73fd74b5f7e44d6f3c0a798149e88a60e6f4176e
MelihDemirtel <melihcandemirtel@gmail.com>
2023-01-21 22:20:23 +0300

    radioButton project done

--- LinkedinProjectsAqua/src/test/java/elementsExamples/RadioButton.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/RadioButton.java
@@ -3,17 +3,24 @@ package elementsExamples;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class RadioButton {
     static int milis = 2000;
-
+    static String actualRadioButtonPageMessage;
+    static String expectedRadioButtonPageMessage = "Do you like the site?";
+    static  String actualRadioYesButtonMessage;
+    static  String expectedRadioYesButtonMessage = "Yes";
+    static  String actualRadioImpressiveButtonMessage;
+    static  String expectedRadioImpressiveButtonMessage = "Impressive";
+    static boolean isClickableNo;
+    RadioButtonPage radioButtonPage = new RadioButtonPage();
     @BeforeClass
     public static void setUpAll() {
         Configuration.browserSize = "1920x1080";
@@ -32,7 +39,36 @@ public class RadioButton {
     }
 
     @Test
-    public void navigatePage() throws InterruptedException {
-        open("https://demoqa.com/elements");
+    public void radioButtonPage() throws InterruptedException {
+        radioButtonPage.elementsButton.click();
+        Thread.sleep(milis);
+
+        radioButtonPage.radioButton.click();
+        Thread.sleep(milis);
+
+        actualRadioButtonPageMessage = radioButtonPage.radioButtonPageMessage.getText();
+        Assert.assertEquals(actualRadioButtonPageMessage, expectedRadioButtonPageMessage);
+
+        radioButtonPage.radioYesButton.click();
+        Thread.sleep(milis);
+
+        actualRadioYesButtonMessage = radioButtonPage.radioYesButtonMessage.getText();
+        Assert.assertEquals(actualRadioYesButtonMessage, expectedRadioYesButtonMessage);
+
+        radioButtonPage.radioImpressiveButton.click();
+        Thread.sleep(milis);
+
+        actualRadioImpressiveButtonMessage = radioButtonPage.radioImpressiveButtonMessage.getText();
+        Assert.assertEquals(actualRadioImpressiveButtonMessage, expectedRadioImpressiveButtonMessage);
+
+        isClickableNo = radioButtonPage.radioNoButton.isEnabled();
+
+        System.out.println("STATE: " + isClickableNo);
+
+        if (isClickableNo){
+            System.out.println("No is not clickable");
+        }else{
+            System.out.println("No is clickable");
+        }
     }
 }
--- LinkedinProjectsAqua/src/test/java/elementsExamples/RadioButtonPage.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/RadioButtonPage.java
@@ -1,4 +1,20 @@
 package elementsExamples;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class RadioButtonPage {
+    public SelenideElement elementsButton = $x("//h5[text()='Elements']");
+    public SelenideElement radioButton = $x("//span[text()='Radio Button']");
+    public SelenideElement radioButtonPageMessage = $(By.cssSelector(".mb-3"));
+    public SelenideElement radioYesButton = $(By.cssSelector("label[for='yesRadio']"));
+    public SelenideElement radioImpressiveButton = $(By.cssSelector("label[for='impressiveRadio']"));
+    public SelenideElement radioNoButton = $(By.cssSelector("label[for='noRadio']"));
+    public SelenideElement radioYesButtonMessage = $(By.className("text-success"));
+    public SelenideElement radioImpressiveButtonMessage = $(By.className("text-success"));
+
+
 }
