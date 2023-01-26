a5efbb8e4e220b9088992aaea3647d0a619df310
MelihDemirtel <melihcandemirtel@gmail.com>
2023-01-26 10:45:48 +0300

    webTables project created

---
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/WebTables.java
@@ -0,0 +1,33 @@
+package elementsExamples;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebTables {
+    static int milis = 1000;
+    WebTablesElements webTablesElements = new WebTablesElements();
+    @BeforeClass
+    public static void setUpAll() {
+        Configuration.browserSize = "1920x1080";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://demoqa.com");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        closeWebDriver();
+    }
+
+    @Test
+    public void test01() throws InterruptedException {
+
+    }
+}
