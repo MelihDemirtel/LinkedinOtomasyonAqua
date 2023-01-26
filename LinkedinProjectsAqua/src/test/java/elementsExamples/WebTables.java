2a2084586999d0721470e61b7100674164438e05
MelihDemirtel <melihcandemirtel@gmail.com>
2023-01-26 22:18:18 +0300

    webTables project done

--- LinkedinProjectsAqua/src/test/java/elementsExamples/WebTables.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/WebTables.java
@@ -9,16 +9,16 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class WebTables {
-    static String scrollDown = "window.scrollBy(0,350)";//Sayfayı aşağı kaydırabilmek için
+
     WebTablesElements webTablesElements = new WebTablesElements();
     @BeforeClass
     public static void setUpAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 5000;
+        Configuration.timeout= 15000;
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         open("https://demoqa.com");
@@ -31,14 +31,66 @@ public class WebTables {
 
     @Test
     public void test01() {
-
+        webTablesElements.elementsButton.shouldHave(Condition.visible);
+        webTablesElements.elementsButton.click();
     }
     @Test
     public void test02() {
-
+        webTablesElements.webTablesButton.click();
     }
     @Test
     public void test03() {
+        webTablesElements.actualFirstRowFirstColm = webTablesElements.rows.get(0).getText();
+        Assert.assertEquals(webTablesElements.actualFirstRowFirstColm, webTablesElements.expectedFirstRowFirstColm);
+    }
+    @Test
+    public void test04() {
+        webTablesElements.searchBox.sendKeys("al");
+        webTablesElements.actualFirstRowFirstColm = webTablesElements.rows.get(0).getText();
+        Assert.assertEquals(webTablesElements.actualFirstRowFirstColm, webTablesElements.expectedFirstRowFirstColmSearch);
+    }
+    @Test
+    public void test05() {
+        webTablesElements.searchBox.clear();
+        webTablesElements.searchBox.sendKeys("zzz");
+        webTablesElements.noRows.shouldHave(Condition.visible);
+        webTablesElements.searchBox.clear();
+    }
+    @Test
+    public void test06() {
+        refresh();
+        webTablesElements.addButton.click();
+    }
+    @Test
+    public void test07() {
+        webTablesElements.firstNameLabel.sendKeys(webTablesElements.firstName);
+        webTablesElements.lastNameLabel.sendKeys(webTablesElements.lastName);
+        webTablesElements.emailLabel.sendKeys(webTablesElements.email);
+        webTablesElements.ageLabel.sendKeys(webTablesElements.age);
+        webTablesElements.salaryLabel.sendKeys(webTablesElements.salary);
+        webTablesElements.departmentLabel.sendKeys(webTablesElements.department);
 
+        webTablesElements.submitButton.click();
+    }
+    @Test
+    public void test08() {
+        webTablesElements.fourthRow.shouldHave(Condition.visible);
+        webTablesElements.actual4Row22Colm = webTablesElements.rows.get(21).getText();
+        Assert.assertEquals(webTablesElements.actual4Row22Colm, webTablesElements.expected4Row22Colm);
+    }
+    @Test
+    public void test09() {
+        webTablesElements.deleteButton1.click();
+        webTablesElements.deleteButton2.click();
+    }
+    @Test
+    public void test10() {
+        webTablesElements.actualFirstRowFirstColmDelete = webTablesElements.rows.get(0).getText();
+        Assert.assertEquals(webTablesElements.actualFirstRowFirstColmDelete, webTablesElements.expectedFirstRowFirstColmDelete);
+    }
+    @Test
+    public void test11() {
+        refresh();
+        webTablesElements.fourthRow.shouldHave(Condition.empty);
     }
 }
--- LinkedinProjectsAqua/src/test/java/elementsExamples/WebTablesElements.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/WebTablesElements.java
@@ -1,7 +1,43 @@
 package elementsExamples;
 
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class WebTablesElements {
+
+    public String expectedFirstRowFirstColm = "Cierra";
+    public String expectedFirstRowFirstColmSearch = "Alden";
+    public String expected4Row22Colm = "Test";
+    public String actual4Row22Colm;
+    public String actualFirstRowFirstColm;
+    public String expectedFirstRowFirstColmDelete = "Kierra";
+    public String actualFirstRowFirstColmDelete;
+    public String firstName = "Test";
+    public String lastName = "Deneme";
+    public String email = "test@deneme.com";
+    public String age = "30";
+    public String salary = "35000";
+    public String department = "Bank";
+    public SelenideElement elementsButton = $x("//h5[text()='Elements']");
+    public SelenideElement webTablesButton = $x("//span[text()='Web Tables']");
+    public ElementsCollection rows = $$(By.className("rt-td"));
+    public SelenideElement searchBox = $(By.id("searchBox"));
+    public SelenideElement noRows = $x("//*[contains(text(),'No rows')]");
+    public SelenideElement addButton = $(By.id("addNewRecordButton"));
+    public SelenideElement firstNameLabel = $(By.id("firstName"));
+    public SelenideElement lastNameLabel = $(By.id("lastName"));
+    public SelenideElement emailLabel = $(By.id("userEmail"));
+    public SelenideElement ageLabel = $(By.id("age"));
+    public SelenideElement salaryLabel = $(By.id("salary"));
+    public SelenideElement departmentLabel = $(By.id("department"));
+    public SelenideElement submitButton = $(By.id("submit"));
+    public SelenideElement fourthRow = $x("(//div[@role='row'])[5]");
+    public SelenideElement deleteButton1 = $(By.id("delete-record-1"));
+    public SelenideElement deleteButton2 = $(By.id("delete-record-2"));
 
 
 }
