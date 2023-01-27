f2d39e75d70ce7e5545806befde57524b12b169b
MelihDemirtel <melihcandemirtel@gmail.com>
2023-01-28 01:40:43 +0300

    mouseButtons project done

--- LinkedinProjectsAqua/src/test/java/elementsExamples/MouseButtons.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/MouseButtons.java
@@ -1,4 +1,61 @@
 package elementsExamples;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
 public class MouseButtons {
+    MouseButtonsElements mouseButtonsElements = new MouseButtonsElements();
+    @BeforeClass
+    public static void SetUpAll() {
+        com.codeborne.selenide.Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 20000;
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://demoqa.com");
+    }
+    @AfterClass
+    public void tearDown(){
+        closeWebDriver();
+    }
+
+    @Test
+    public void test01() {
+        mouseButtonsElements.elementsButton.shouldBe(Condition.interactable);
+        mouseButtonsElements.elementsButton.click();
+    }
+    @Test
+    public void test02() {
+        mouseButtonsElements.buttonsButtons.shouldBe(Condition.interactable);
+        mouseButtonsElements.buttonsButtons.click();
+    }
+    @Test
+    public void test03() {
+        mouseButtonsElements.doubleClickBtn.doubleClick();
+        System.out.println(mouseButtonsElements.actualDoubleClickMessage.getText());
+        mouseButtonsElements.actualDoubleClickMessage.shouldBe(Condition.visible);
+        Assert.assertEquals(mouseButtonsElements.actualDoubleClickMessage.getText(), mouseButtonsElements.expectedDoubleClickMessage);
+    }
+    @Test
+    public void test04() {
+        mouseButtonsElements.rightClickBtn.contextClick();
+        System.out.println(mouseButtonsElements.actualRightClickMessage.getText());
+        mouseButtonsElements.actualRightClickMessage.shouldBe(Condition.visible);
+        Assert.assertEquals(mouseButtonsElements.actualRightClickMessage.getText(), mouseButtonsElements.expectedRightClickMessage);
+    }
+    @Test
+    public void test05() {
+        mouseButtonsElements.clickBtn.click();
+        System.out.println(mouseButtonsElements.actualDynamicClickMessage.getText());
+        mouseButtonsElements.actualDynamicClickMessage.shouldBe(Condition.visible);
+        Assert.assertEquals(mouseButtonsElements.actualDynamicClickMessage.getText(), mouseButtonsElements.expectedDynamicClickMessage);
+    }
 }
--- LinkedinProjectsAqua/src/test/java/elementsExamples/MouseButtonsElements.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/MouseButtonsElements.java
@@ -1,4 +1,29 @@
 package elementsExamples;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class MouseButtonsElements {
+    public SelenideElement elementsButton = $x("//h5[text()='Elements']");
+    public SelenideElement buttonsButtons = $x("//span[text()='Buttons']");
+    public SelenideElement doubleClickBtn = $(By.id("doubleClickBtn"));
+    public SelenideElement rightClickBtn = $(By.id("rightClickBtn"));
+    public SelenideElement clickBtn = $x("//button[text()='Click Me']");
+    public SelenideElement actualDoubleClickMessage = $(By.id("doubleClickMessage"));
+    public SelenideElement actualRightClickMessage = $(By.id("rightClickMessage"));
+    public SelenideElement actualDynamicClickMessage = $(By.id("dynamicClickMessage"));
+
+
+    //Strings
+    public String expectedDoubleClickMessage = "You have done a double click";
+    public String expectedRightClickMessage = "You have done a right click";
+    public String expectedDynamicClickMessage = "You have done a dynamic click";
+
+
+
+
+
 }
