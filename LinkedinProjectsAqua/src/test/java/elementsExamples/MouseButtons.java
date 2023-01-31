ece40071598cb87d0929ba3b2392b418f478c3ab
MelihDemirtel <melihcandemirtel@gmail.com>
2023-01-31 20:48:59 +0300

    mouseButtons project updated

--- LinkedinProjectsAqua/src/test/java/elementsExamples/MouseButtons.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/MouseButtons.java
@@ -16,7 +16,7 @@ public class MouseButtons {
     MouseButtonsElements mouseButtonsElements = new MouseButtonsElements();
     @BeforeClass
     public static void SetUpAll() {
-        com.codeborne.selenide.Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1920x1080";
         Configuration.timeout = 20000;
         SelenideLogger.addListener("allure", new AllureSelenide());
 
@@ -26,36 +26,40 @@ public class MouseButtons {
     public void tearDown(){
         closeWebDriver();
     }
-
     @Test
     public void test01() {
+        mouseButtonsElements.elementsButton.shouldHave(Condition.visible);
         mouseButtonsElements.elementsButton.shouldBe(Condition.interactable);
         mouseButtonsElements.elementsButton.click();
     }
     @Test
     public void test02() {
-        mouseButtonsElements.buttonsButtons.shouldBe(Condition.interactable);
-        mouseButtonsElements.buttonsButtons.click();
+        mouseButtonsElements.buttonsButton.shouldHave(Condition.visible);
+        mouseButtonsElements.buttonsButton.shouldBe(Condition.interactable);
+        mouseButtonsElements.buttonsButton.click();
     }
     @Test
     public void test03() {
+        mouseButtonsElements.doubleClickBtn.shouldBe(Condition.interactable);
         mouseButtonsElements.doubleClickBtn.doubleClick();
+        mouseButtonsElements.actualDoubleClickMessage.shouldHave(Condition.visible);
         System.out.println(mouseButtonsElements.actualDoubleClickMessage.getText());
-        mouseButtonsElements.actualDoubleClickMessage.shouldBe(Condition.visible);
         Assert.assertEquals(mouseButtonsElements.actualDoubleClickMessage.getText(), mouseButtonsElements.expectedDoubleClickMessage);
     }
     @Test
     public void test04() {
+        mouseButtonsElements.rightClickBtn.shouldBe(Condition.interactable);
         mouseButtonsElements.rightClickBtn.contextClick();
+        mouseButtonsElements.actualRightClickMessage.shouldHave(Condition.visible);
         System.out.println(mouseButtonsElements.actualRightClickMessage.getText());
-        mouseButtonsElements.actualRightClickMessage.shouldBe(Condition.visible);
         Assert.assertEquals(mouseButtonsElements.actualRightClickMessage.getText(), mouseButtonsElements.expectedRightClickMessage);
     }
     @Test
     public void test05() {
-        mouseButtonsElements.clickBtn.click();
+        mouseButtonsElements.dynamicClickBtn.shouldBe(Condition.interactable);
+        mouseButtonsElements.dynamicClickBtn.click();
+        mouseButtonsElements.actualDynamicClickMessage.shouldHave(Condition.visible);
         System.out.println(mouseButtonsElements.actualDynamicClickMessage.getText());
-        mouseButtonsElements.actualDynamicClickMessage.shouldBe(Condition.visible);
         Assert.assertEquals(mouseButtonsElements.actualDynamicClickMessage.getText(), mouseButtonsElements.expectedDynamicClickMessage);
     }
 }
--- LinkedinProjectsAqua/src/test/java/elementsExamples/MouseButtonsElements.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/MouseButtonsElements.java
@@ -8,22 +8,16 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class MouseButtonsElements {
     public SelenideElement elementsButton = $x("//h5[text()='Elements']");
-    public SelenideElement buttonsButtons = $x("//span[text()='Buttons']");
+    public SelenideElement buttonsButton = $x("//span[text()='Buttons']");
     public SelenideElement doubleClickBtn = $(By.id("doubleClickBtn"));
     public SelenideElement rightClickBtn = $(By.id("rightClickBtn"));
-    public SelenideElement clickBtn = $x("//button[text()='Click Me']");
+    public SelenideElement dynamicClickBtn = $x("//button[text()='Click Me']");
     public SelenideElement actualDoubleClickMessage = $(By.id("doubleClickMessage"));
     public SelenideElement actualRightClickMessage = $(By.id("rightClickMessage"));
     public SelenideElement actualDynamicClickMessage = $(By.id("dynamicClickMessage"));
 
-
     //Strings
     public String expectedDoubleClickMessage = "You have done a double click";
     public String expectedRightClickMessage = "You have done a right click";
-    public String expectedDynamicClickMessage = "You have done a dynamic click";
-
-
-
-
-
+    public String expectedDynamicClickMessage  = "You have done a dynamic click";
 }
