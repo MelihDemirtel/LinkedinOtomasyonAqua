fbe022d73f6b204dcdd79475f49d27548fc471bc
MelihDemirtel <melihcandemirtel@gmail.com>
2023-02-06 00:51:11 +0300

    links project continue

--- LinkedinProjectsAqua/src/test/java/elementsExamples/Links.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/Links.java
@@ -1,14 +1,19 @@
 package elementsExamples;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class Links {
     LinksElements linksElements = new LinksElements();
@@ -26,6 +31,38 @@ public class Links {
     }
     @Test
     public void test01() {
-
+        linksElements.elementsButton.click();
+    }
+    @Test
+    public void test02() {
+        linksElements.linksButton.click();
+    }
+    @Test
+    public void test03() throws InterruptedException {
+        linksElements.homeLink.click();
+        linksElements.windows = Selenide.webdriver().object().getWindowHandles();
+        linksElements.window = linksElements.windows.iterator();
+        linksElements.parentId = linksElements.window.next();
+        linksElements.childIdOne = linksElements.window.next();
+        Selenide.switchTo().window(linksElements.childIdOne);
+        linksElements.elementsButton.shouldHave(Condition.visible).wait(5000);
+        closeWindow();
+        Selenide.switchTo().window(linksElements.parentId);
+    }
+    @Test
+    public void test04() {
+        for (int i = 4; i < linksElements.links.size(); i++) {
+            linksElements.url = linksElements.links.get(i).getAttribute("href");
+            linksElements.urlText = linksElements.links.get(i).getText();
+            try {
+                linksElements.connection = (HttpURLConnection) new URL(linksElements.url).openConnection();
+                linksElements.connection.setRequestMethod("HEAD");
+                linksElements.connection.connect();
+                linksElements.responseCode = linksElements.connection.getResponseCode();
+                System.out.println("URL: " + linksElements.url + " | " + "Returned Response Code: " + linksElements.responseCode + " | " + "URL Text: " + linksElements.urlText);
+            } catch (IOException e) {
+                System.out.println("Error Checking Response Code For URL: " + linksElements.url + " | " + "URL Text: " + linksElements.urlText);
+            }
+        }
     }
 }
--- LinkedinProjectsAqua/src/test/java/elementsExamples/LinksElements.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/LinksElements.java
@@ -1,4 +1,31 @@
 package elementsExamples;
 
+import com.codeborne.selenide.*;
+import org.apache.hc.core5.util.Timeout;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.HttpURLConnection;
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class LinksElements {
+    public SelenideElement elementsButton = $x("//h5[text()='Elements']");
+    public SelenideElement linksButton = $x("//span[text()='Links']");
+    public SelenideElement homeLink = $(By.linkText("Home"));
+    public ElementsCollection links = $$(By.tagName("a"));
+    public Set<String> windows;
+    public Iterator<String> window;
+    public String parentId;
+    public  String childIdOne;
+    public String url;
+    public String urlText;
+    public HttpURLConnection connection;
+    public int responseCode;
 }
