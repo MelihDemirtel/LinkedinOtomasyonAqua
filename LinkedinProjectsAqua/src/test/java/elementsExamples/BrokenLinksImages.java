aab344a98f9e737249f514a17decd6b321ed6570
MelihDemirtel <melihcandemirtel@gmail.com>
2023-02-10 23:35:01 +0300

    updated links and continue brokenLinks

--- LinkedinProjectsAqua/src/test/java/elementsExamples/BrokenLinksImages.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/BrokenLinksImages.java
@@ -6,12 +6,14 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BrokenLinksImages {
     BrokenLinksImagesElements brokenElements = new BrokenLinksImagesElements();
+    SoftAssert softAssert = new SoftAssert();
     @BeforeClass
     public static void SetUpAll() {
         Configuration.browserSize = "1920x1080";
--- LinkedinProjectsAqua/src/test/java/elementsExamples/BrokenLinksImagesElements.java
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/BrokenLinksImagesElements.java
@@ -1,4 +1,25 @@
 package elementsExamples;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import java.net.HttpURLConnection;
+
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class BrokenLinksImagesElements {
+    //ELEMENTS
+    public SelenideElement elementsButton = $x("//h5[text()='Elements']");
+    public SelenideElement brokenLinksButton = $x("//span[@class='text' and contains(text(),'Broken')]");
+
+    //OTHER
+    public ElementsCollection images = $$(By.tagName("img"));
+    public ElementsCollection links = $$(By.tagName("a"));
+
+    //HTTP
+    public HttpURLConnection connection;
+    public int responseCodeImg;
+    public int getResponseCodeLink;
 }
