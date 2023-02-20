3e5a4c494e115fcbd26fc971aca6fc1adc23c17a
MelihDemirtel <melihcandemirtel@gmail.com>
2023-02-20 10:43:15 +0300

    dynamicProperties project created

---
+++ LinkedinProjectsAqua/src/test/java/elementsExamples/DynamicProperties.java
@@ -0,0 +1,44 @@
+package elementsExamples;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DynamicProperties {
+    DynamicPropertiesElements elements = new DynamicPropertiesElements();
+    @BeforeClass
+    public static void SetUpAll() {
+        Configuration.browserSize = "1910x1020";
+        Configuration.timeout = 20000;
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://demoqa.com");
+    }
+    @AfterClass
+    public void tearDown(){
+        closeWebDriver();
+    }
+    @Test
+    public void test01() {
+
+    }
+    @Test
+    public void test02() {
+
+    }
+    @Test
+    public void test03() {
+
+    }
+    @Test
+    public void test04() {
+
+    }
+
+    }
